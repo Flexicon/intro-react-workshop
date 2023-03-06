d0db8a4b494ec499a1f749a7953486dc31ee3507
Mike Repec <mikerepec@gmail.com>
2023-03-06 22:23:48 +0100

    First take on Typescript bullet points

--- pages/3.tsx
+++ pages/3.tsx
@@ -1,9 +1,54 @@
+import CodeSnippet from 'components/CodeSnippetLoader'
+
 export default function Ex3() {
   return (
     <>
       <h1>TypeScript</h1>
 
-      <p>TODO...</p>
+      <h3>Primitive types</h3>
+
+      <CodeSnippet>
+        {`
+const name: string = 'Neo'
+const age: number = 37
+const isInTheKnow: boolean = true
+
+// Type annotations for variable declaration are optional
+const isReallyInTheKnow = false // TypeScript will infer as much as it can
+      `.trim()}
+      </CodeSnippet>
+
+      <h3>Types and interfaces</h3>
+
+      <CodeSnippet>
+        {`
+type PersonT = {
+  name: string
+  age: number
+  isInTheKnow: boolean
+}
+
+interface PersonI {
+  name: string
+  age: number
+  isInTheKnow: boolean
+}
+
+let p1: PersonT = {
+  name: 'Neo',
+  age: 37,
+  isInTheKnow: true,
+}
+
+const p2: PersonI = {
+  name: 'Neo',
+  age: 37,
+  isInTheKnow: true,
+}
+
+p1 = p2 // This is okay because the types overlap
+      `.trim()}
+      </CodeSnippet>
     </>
   )
 }
