950962d3fefc38e6ecffc0e15001e11bf70dcc28
Mike Repec <mikerepec@gmail.com>
2023-03-07 11:10:01 +0100

    Add example useCallback component

--- components/ExampleUseCallback.tsx
+++ components/ExampleUseCallback.tsx
@@ -1,4 +1,32 @@
-import { useCallback, useState } from 'react'
+import { memo, useCallback, useState } from 'react'
 
-// TODO: implement useCallback example
-export default function ExampleUseCallback() {}
+import styles from 'styles/ExampleUseCallback.module.css'
+
+const SomeComponent = ({}: { onEvent: () => void }) => {
+  console.log('<SomeComponent /> rendering')
+
+  return <strong className={styles.someComponent}>This is SomeComponent</strong>
+}
+
+export default function ExampleUseCallback() {
+  const [count, setCount] = useState(42)
+
+  // This is regular callback function and can used safely for direct html events
+  // and in general internally in the component
+  const onClick = () => {
+    setCount(count * Math.floor(1 + Math.random() * 13))
+  }
+
+  // When needing to pass your callback to a child component you usually want to use the
+  // useCallback hook in order to avoid needless re-renders
+  const onEvent = useCallback(() => {
+    // Code here will run when the onEvent callback is invoked
+  }, [])
+
+  return (
+    <div className={styles.wrapper}>
+      <button onClick={onClick}>Stonks: {count}</button>
+      <SomeComponent onEvent={onEvent} />
+    </div>
+  )
+}
