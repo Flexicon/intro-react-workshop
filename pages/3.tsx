da8db538490edf66a0052bbd1f12e750c9717f61
Mike Repec <mikerepec@gmail.com>
2023-03-07 11:51:31 +0100

    Add more typescript overviews

--- pages/3.tsx
+++ pages/3.tsx
@@ -3,51 +3,108 @@ import CodeSnippet from 'components/CodeSnippetLoader'
 export default function Ex3() {
   return (
     <>
-      <h1>TypeScript</h1>
+      <h1>TypeScript Speedrun 🏎️</h1>
 
-      <h3>Primitive types</h3>
+      <h3>🧌 Primitive types</h3>
 
       <CodeSnippet>
         {`
 const name: string = 'Neo'
 const age: number = 37
-const isInTheKnow: boolean = true
+const isHuman: boolean = true
 
-// Type annotations for variable declaration are optional
-const isReallyInTheKnow = false // TypeScript will infer as much as it can
+// Type annotations for variable declaration is optional
+const isInTheKnow = false // TypeScript will usually be able to infer your type
       `.trim()}
       </CodeSnippet>
 
-      <h3>Types and interfaces</h3>
+      <h3>🪞 Types and Interfaces</h3>
 
       <CodeSnippet>
         {`
 type PersonT = {
   name: string
   age: number
-  isInTheKnow: boolean
+  isHuman: boolean
 }
 
 interface PersonI {
   name: string
   age: number
-  isInTheKnow: boolean
+  isHuman: boolean
+
+  makeChoice(): void
 }
 
 let p1: PersonT = {
-  name: 'Neo',
-  age: 37,
-  isInTheKnow: true,
+  name: 'Agent Smith',
+  age: NaN, // Fun fact: "Not a Number" is technically a number 🧙🏻
+  isHuman: false,
 }
 
-const p2: PersonI = {
+let p2: PersonI = {
   name: 'Neo',
   age: 37,
-  isInTheKnow: true,
+  isHuman: true,
+  makeChoice: () => {},
 }
 
 p1 = p2 // This is okay because the types overlap
+p2 = p1 // This is not because 👇
+// TS Error: Property 'makeChoice' is missing in type 'PersonT' but required in type 'PersonI'.
       `.trim()}
+      </CodeSnippet>
+
+      <h3>🤖 Generics</h3>
+
+      <CodeSnippet>
+        {`
+function Sample<T>(values: T[]): T {
+  return values[Math.floor(Math.random() * values.length)]
+}
+
+class LinkedListNode<T> {
+  constructor(public data: T, public next: LinkedListNode<T>) {}
+}
+        `.trim()}
+      </CodeSnippet>
+
+      <h3>💰 Inheritance</h3>
+
+      <CodeSnippet>
+        {`
+interface Game {
+  name: string
+}
+
+class Blackjack implements Game {
+  name = 'Blackjack'
+}
+
+class Poker implements Game {
+  name = 'Poker'
+  hand: number = 0
+}
+
+class Roulette implements Game {
+  name = 'Poker'
+}
+
+interface Gambler<T> {
+  name: string
+  game: T
+}
+
+const morpheus = {
+  name: 'Morpheus',
+  game: new Blackjack(),
+}
+
+// Fails: Property 'hand' is missing in type 'Blackjack' but required in type 'Poker'.
+const neo: Gambler<Poker> = morpheus
+// This is fine, because the properties overlap
+const trinity: Gambler<Roulette> = morpheus
+        `.trim()}
       </CodeSnippet>
     </>
   )
