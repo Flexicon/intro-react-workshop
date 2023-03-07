dea8c2ed4e2cd9e936309059c09579598c254e89
Mike Repec <mikerepec@gmail.com>
2023-03-07 22:57:25 +0100

    Split PancakeMaker tests up into individual test cases

--- components/PancakeMaker.test.tsx
+++ components/PancakeMaker.test.tsx
@@ -1,44 +1,71 @@
-import { expect, test } from 'vitest'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, beforeEach, describe, expect, test } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 import PancakeMaker from './PancakeMaker'
 
 const pancake = '🥞'
 
-test('PancakeMaker', async () => {
-  render(<PancakeMaker />)
+describe('PancakeMaker', () => {
+  let makeBtn: HTMLElement, eatBtn: HTMLElement, pancakes: HTMLElement
 
-  const makeBtn = screen.getByText('Make Pancake')
-  const eatBtn = screen.getByText('Eat Pancake')
-  const pancakes = screen.getByTestId('pancakes')
+  beforeEach(() => {
+    render(<PancakeMaker />)
+    makeBtn = screen.getByText('Make Pancake')
+    eatBtn = screen.getByText('Eat Pancake')
+    pancakes = screen.getByTestId('pancakes')
+  })
 
-  expect(pancakes.textContent).toBe(pancake.repeat(3))
+  afterEach(() => {
+    cleanup()
+  })
 
-  fireEvent.click(makeBtn)
-  expect(pancakes.textContent).toBe(pancake.repeat(4))
+  test('should make pancakes', async () => {
+    expect(pancakes.textContent).toBe(pancake.repeat(3))
 
-  fireEvent.click(eatBtn)
-  fireEvent.click(eatBtn)
-  expect(pancakes.textContent).toBe(pancake.repeat(2))
-
-  fireEvent.click(eatBtn)
-  fireEvent.click(eatBtn)
-  expect(pancakes.textContent).toContain('...no more pancakes')
-
-  fireEvent.click(eatBtn)
-  fireEvent.click(eatBtn)
-  expect(pancakes.textContent).toContain('...no more pancakes')
-
-  fireEvent.click(makeBtn)
-  expect(pancakes.textContent).toBe(pancake.repeat(1))
-
-  for (let i = 1; i < 10; i++) {
     fireEvent.click(makeBtn)
-  }
-  expect(pancakes.textContent).toBe(pancake.repeat(10))
-  expect(
-    screen.queryByText("✋ Woah there, that's plenty pancakes there bud.")
-  ).not.toBeNull()
+    expect(pancakes.textContent).toBe(pancake.repeat(4))
 
-  fireEvent.click(makeBtn)
-  expect(pancakes.textContent).toBe(pancake.repeat(10))
+    fireEvent.click(makeBtn)
+    fireEvent.click(makeBtn)
+    expect(pancakes.textContent).toBe(pancake.repeat(6))
+  })
+
+  test('should eat pancakes', async () => {
+    expect(pancakes.textContent).toBe(pancake.repeat(3))
+
+    fireEvent.click(eatBtn)
+    expect(pancakes.textContent).toBe(pancake.repeat(2))
+
+    fireEvent.click(eatBtn)
+    fireEvent.click(eatBtn)
+    expect(pancakes.textContent).toContain('...no more pancakes')
+  })
+
+  test('should make pancakes after eating all of them', async () => {
+    expect(pancakes.textContent).toBe(pancake.repeat(3))
+
+    for (let i = 0; i < 10; i++) {
+      fireEvent.click(eatBtn)
+    }
+    expect(pancakes.textContent).toContain('...no more pancakes')
+
+    fireEvent.click(makeBtn)
+    expect(pancakes.textContent).toBe(pancake)
+  })
+
+  test('should only make pancakes up to limit', async () => {
+    expect(pancakes.textContent).toBe(pancake.repeat(3))
+
+    for (let i = 0; i < 7; i++) {
+      fireEvent.click(makeBtn)
+    }
+    expect(pancakes.textContent).toBe(pancake.repeat(10))
+
+    fireEvent.click(makeBtn)
+    expect(pancakes.textContent).toBe(pancake.repeat(10))
+
+    for (let i = 0; i < 20; i++) {
+      fireEvent.click(makeBtn)
+    }
+    expect(pancakes.textContent).toBe(pancake.repeat(10))
+  })
 })
